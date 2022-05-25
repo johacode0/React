ab2d513caad1f359468d7e0de7c8011d82a2fa67
JOHACODE <97564854+johacode0@users.noreply.github.com>
2022-05-25 14:11:32 +0200

    Update Navbar.js

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div>
     <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-    <a className="navbar-brand" href="/">Git engine</a>
+    <a className="navbar-brand" href="/">JOHACODE</a>
     <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="collapsibleNavId">
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-                <a className="nav-link" href="/users">Users <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/users">Users <span className="sr-only">(git)</span></a>
             </li>
         </ul>
     </div>
